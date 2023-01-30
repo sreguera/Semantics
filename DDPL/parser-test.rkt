bfd7a0e743adea08a74d02aea8fec65cfef8ba4a
sreguera <sreguera@gmail.com>
2023-01-30 21:42:07 +0100

    Parser for DDPL

---
+++ DDPL/parser-test.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+(require rackunit)
+(require parser-tools/lex)
+(require "parser.rkt")
+
+(define (get-tokens a-lexer a-string)
+  (define input (open-input-string a-string))
+  (define (extract token)
+    (if (token? token)
+        (list (token-name token) (token-value token))
+        token))
+  (define (get-all-tokens acc)
+    (let ([token (a-lexer input)])
+      (if (equal? token 'EOF)
+          (reverse (cons (extract token) acc))
+          (get-all-tokens (cons (extract token) acc)))))
+  (get-all-tokens empty))
+
+(check-equal?
+ (get-tokens ddpl-lexer "( ) if then else while do ; := + = not output read")
+ '(LPAREN RPAREN IF THEN ELSE WHILE DO SEMI ASSIGN PLUS EQ NOT OUTPUT READ EOF)
+ "Simple tokens are scanned.")
+
+(check-equal?
+ (get-tokens ddpl-lexer "abcd 0 1 true false") 
+ '((ID abcd) (NUM 0) (NUM 1) (BOOL #t) (BOOL #f) EOF)
+ "Complex tokens are scanned.")
+
+(check-equal?
+ (parse-string "a := 0; a := 1")
+ '(SEQ (ASSIGN a 0) (ASSIGN a 1))
+ "Parsing assignments")
+
+(check-equal?
+ (parse-string "while true do a := 0; a := 1")
+ '(WHILE #t (SEQ (ASSIGN a 0) (ASSIGN a 1)))
+ "Parsing while")
---
+++ DDPL/parser.rkt
@@ -0,0 +1,58 @@
+#lang racket
+
+(require parser-tools/lex)
+(require (prefix-in : parser-tools/lex-sre))
+(require parser-tools/yacc)
+
+(provide parse-string (protect-out ddpl-parser ddpl-lexer))
+
+;; Instead of 1 0 true false we have NUM and BOOL
+(define-tokens data-tokens (NUM BOOL ID))
+(define-empty-tokens punct-tokens (EOF IF THEN ELSE WHILE DO LPAREN RPAREN SEMI PLUS ASSIGN EQ NOT OUTPUT READ IMP))
+
+(define ddpl-lexer
+  (lexer
+   [(eof) (token-EOF)]
+   ["(" (token-LPAREN)]
+   [")" (token-RPAREN)]
+   ["if" (token-IF)]
+   ["then" (token-THEN)]
+   ["else" (token-ELSE)]
+   ["while" (token-WHILE)]
+   ["do" (token-DO)]
+   [";" (token-SEMI)]
+   ["+" (token-PLUS)]
+   [":=" (token-ASSIGN)]
+   ["=" (token-EQ)]
+   ["not" (token-NOT)]
+   ["output" (token-OUTPUT)]
+   ["read" (token-READ)]
+   ["true" (token-BOOL #t)]
+   ["false" (token-BOOL #f)]
+   [(:+ numeric) (token-NUM (string->number lexeme))]
+   [(:: alphabetic
+        (:* (:or alphabetic numeric #\_)))
+    (token-ID (string->symbol lexeme))]
+   [whitespace (ddpl-lexer input-port)]))
+
+(define ddpl-parser
+  (parser
+   [start cmd]
+   [end EOF]
+   [error void]
+   [tokens data-tokens punct-tokens]
+   [grammar
+    [cmd [(ID ASSIGN expr) (list 'ASSIGN $1 $3)]
+         [(OUTPUT expr) (list 'OUTPUT $2)]
+         [(IF expr THEN cmd ELSE cmd) (list 'IF $2 $4 $6)]
+         [(WHILE expr DO cmd) (list 'WHILE $2 $4)]
+         [(cmd SEMI cmd) (list 'SEQ $1 $3)]
+         [(LPAREN cmd RPAREN) $2]]
+    [expr [(LPAREN expr RPAREN) $2]
+          [(NUM) $1]
+          [(BOOL) $1]
+          [(ID) $1]]]))
+
+(define (parse-string s)
+  (define p (open-input-string s))
+  (ddpl-parser (λ () (ddpl-lexer p))))
