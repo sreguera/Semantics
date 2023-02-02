9f60c8614ed0cffa0d78789aeae16e2efaa150e7
sreguera <sreguera@gmail.com>
2023-02-02 21:37:49 +0100

    If and while implementation.

--- DDPL/tiny-eval.rkt
+++ DDPL/tiny-eval.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(provide c-eval make-state (struct-out state))
+
 (struct state (memory input output) #:transparent)
 
 (define (make-state)
@@ -14,14 +16,14 @@
        (match (hash-ref m x 'unbound)
          ['unbound 'error]
          [v (list v s)]))]
-    [(list 'NOT e1)
+    [`(NOT ,e1)
      (match (e-eval e1 s)
        [(list v1 s1)
         (if (boolean? v1)
             (list (not v1) s1)
             'error)]
        [_ 'error])]
-    [(list 'EQ e1 e2)
+    [`(EQ ,e1 ,e2)
      (match (e-eval e1 s)
        [(list v1 s1)
         (match (e-eval e2 s1)
@@ -29,7 +31,7 @@
            (list (equal? v1 v2) s2)]
           [_ 'error])]
         [_ 'error])]
-    [(list 'PLUS e1 e2)
+    [`(PLUS ,e1 ,e2)
      (match (e-eval e1 s)
        [(list v1 s1)
         (match (e-eval e2 s1)
@@ -42,17 +44,37 @@
 
 (define (c-eval c s)
   (match c
-    [(list 'ASSIGN x e)
+    [`(ASSIGN ,x ,e)
      (match (e-eval e s)
        [(list v (state m i o))
         (state (hash-set m x v) i o)]
        [_ 'error])]
-    [(list 'OUTPUT e)
+    [`(OUTPUT ,e)
      (match (e-eval e s)
        [(list v (state m i o))
         (state m i (cons v o))]
        [_ 'error])]
-    [(list 'SEQ c1 c2)
+    [`(IF ,e ,c1, c2)
+     (match (e-eval e s)
+       [(list v s1)
+        (if (boolean? v)
+            (if v
+                (c-eval c1 s1)
+                (c-eval c2 s1))
+            'error)]
+       [_ 'error])]
+    [`(WHILE ,e ,c)
+     (match (e-eval e s)
+       [(list v s1)
+        (if (boolean? v)
+            (if v
+                (match (c-eval c s1)
+                  ['error 'error]
+                  [s2 (c-eval `(WHILE ,e ,c) s2)])
+                s1)
+            'error)]
+       [_ 'error])]
+    [`(SEQ ,c1 ,c2)
      (match (c-eval c1 s)
        ['error 'error]
        [s1 (c-eval c2 s1)])]))
